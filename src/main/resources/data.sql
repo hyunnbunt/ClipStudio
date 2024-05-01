f3a501361f38e590dbd5e5df13f1f97b1f9fd2bf
hyunnbunt <rha542@gmail.com>
2024-05-01 16:04:05 +0900

    deleted data.sql

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into users(username, email) values('hyuna', 'hyuu@g.com');
-insert into videos(title, uploader_number, duration, created_date, total_views, daily_views) values('title', 1, 610, CURRENT_TIMESTAMP, 100, 0);
-insert into advertisements(video_number, order_in_video, total_views, daily_views) values(1, 1, 50, 0);
-insert into advertisements(video_number, order_in_video, total_views, daily_views) values(1, 2, 50, 0);
