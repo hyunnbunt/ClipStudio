e47df387bb8d0babfb1671c5750f865b4b965225
hyunnbunt <rha542@gmail.com>
2024-05-16 16:53:49 +0900

    refactored DailyProfitOfVideoService

--- api/src/main/java/clipstudio/service/DailyProfitOfVideoService.java
+++ api/src/main/java/clipstudio/service/DailyProfitOfVideoService.java
@@ -29,58 +29,6 @@ public class DailyProfitOfVideoService {
     private final AdvertisementRepository advertisementRepository;
     private final DailyProfitOfAdvertisementRepository dailyProfitOfAdvertisementRepository;
     private final DailyProfitOfVideoRepository dailyProfitOfVideoRepository;
-    public List<DailyProfitOfVideo> showDailyProfitOfVideos(String userEmail, LocalDate date) throws IllegalArgumentException {
-        log.info("inside DailyProfitOfVideoService");
-        User user = userRepository.findByEmail(userEmail).orElseThrow();
-        List<Video> videoList = videoRepository.findByUploader(user).orElse(null); // 사용자가 업로드한 모든 동영상 목록 불러오기
-        if (videoList == null) {
-            log.info("You don't have any uploaded video.");
-            throw new IllegalArgumentException();
-        }
-        log.info(String.valueOf(videoList.size()));
-        List<DailyProfitOfVideo> resultList = new LinkedList<>(); // 동영상 각각의 해당 날짜 정산 내역을 찾아 목록 생성
-        for (Video video : videoList) {
-            // 해당 날짜에 정산 내역이 있는 동영상만 목록에 추가
-            dailyProfitOfVideoRepository.findByVideoNumberAndCalculatedDate(video.getNumber(), date).ifPresent(resultList::add);
-        }
-        if (resultList.isEmpty()) {
-            log.info("Can't find your profit statistic on " + date.toString());
-        }
-        return resultList;
-    }
-
-    public List<TotalAdvertisementsProfitOfVideoDto> showDailyProfitOfAdvertisements(String userEmail, LocalDate date) throws IllegalArgumentException {
-        log.info("inside DailyProfitOfVideoService");
-        User user = userRepository.findByEmail(userEmail).orElseThrow();
-        List<Video> videoList = videoRepository.findByUploader(user).orElse(null); // 사용자가 업로드한 모든 동영상 목록 불러오기
-        List<TotalAdvertisementsProfitOfVideoDto> totalAdvertisementsProfitOfVideoList = new LinkedList<>();
-        if (videoList == null) { // 사용자 업로드 비디오가 없다면 exception 던지기
-            log.info("You don't have any uploaded video.");
-            throw new IllegalArgumentException();
-        }
-        for (Video video : videoList) {
-            List<Advertisement> advertisementList = advertisementRepository
-                    .findByVideoNumber(video.getNumber())
-                    .orElse(null); // 사용자 업로드 동영상별 광고 목록 가져오기
-            if (advertisementList != null) { // 광고 목록이 존재한다면, 광고 목록을 순회하며, 각 광고 넘버와 조회 날짜로 광고 정산 내역을 조회할 것
-                double totalAdvertisementsProfit = 0;
-                for (Advertisement advertisement : advertisementList) {
-                    DailyProfitOfAdvertisement dailyProfitOfAdvertisement = dailyProfitOfAdvertisementRepository.
-                            findByAdvertisementNumberAndCalculatedDate(advertisement.getNumber(), date); // 각 광고별 정산 내역을 조회
-                    totalAdvertisementsProfit += dailyProfitOfAdvertisement.getDailyProfit(); // 각 광고별 정산 금액을 합산
-                }
-                // 각 비디오마다 총 광고 정산 금액을 담은 dto를 생성
-                TotalAdvertisementsProfitOfVideoDto totalAdvertisementsProfitOfVideo =
-                        TotalAdvertisementsProfitOfVideoDto.builder()
-                                .totalProfitOfAllAdvertisementsInVideo(totalAdvertisementsProfit)
-                                .videoNumber(video.getNumber())
-                                .calculatedDate(date)
-                                .build();
-                totalAdvertisementsProfitOfVideoList.add(totalAdvertisementsProfitOfVideo); // 각 비디오별 총 광고 정산 금액 dto를 목록에 추가
-            }
-        }
-        return totalAdvertisementsProfitOfVideoList;
-    }
 
     public List<DailyProfitDto> showDailyProfit(String userEmail, LocalDate date) {
         User user = userRepository.findByEmail(userEmail).orElseThrow();
